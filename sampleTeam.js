10534415e3cadbe78b203ced2fc1f3e160d66d01
Roly61-hub <garyrrohloff@gmail.com>
2025-11-21 11:54:27 +1300

    Delete sampleTeam.js

--- sampleTeam.js
+++
@@ -1,7 +0,0 @@
-
-export const SAMPLE_TEAM = [
-  { name: "Alex", role: "CEO / Founder", mbti: "ENTJ" },
-  { name: "Jamie", role: "Product Lead", mbti: "INFP" },
-  { name: "Riley", role: "Operations", mbti: "ISTJ" },
-  { name: "Sam", role: "Growth", mbti: "ENFP" },
-];
